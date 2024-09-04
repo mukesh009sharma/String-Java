8e407e6d51f57fa0e1dab014dd2212c0680acf88
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-09-04 22:11:11 +0530

    Add files via upload

---
+++ _4LastCharacterstic.java
@@ -0,0 +1,8 @@
+public class _4LastCharacterstic {
+ public static void main(String[] args) {
+    String s1="Inelligence";
+    //Acessing the last character
+    char lastchar=s1.charAt(s1.length()-1);
+    System.out.println("The last character of the String is : "+lastchar);
+ }   
+}
