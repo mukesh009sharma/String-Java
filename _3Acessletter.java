41f5127894a6d00524f150695a079f57c20aead0
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-09-04 22:10:48 +0530

    Add files via upload

---
+++ _3Acessletter.java
@@ -0,0 +1,9 @@
+public class _3Acessletter{
+    public static void main(String[] args) {
+        String s1="helloo";
+        //Acessing the 1st letter
+       char firstchar=s1.charAt(1);
+        System.out.println("The first character of the string is: " + firstchar);
+        
+    }
+}
