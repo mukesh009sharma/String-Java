b55547fbe0d1432587e03f1e11c5c2c2272540a6
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-09-05 22:34:51 +0530

    Add files via upload

---
+++ _8Uppercase.java
@@ -0,0 +1,9 @@
+public class _8Uppercase {
+    
+        public static void main(String[] args) {
+            String str = "world";
+            String upperStr = str.toUpperCase();
+            System.out.println(upperStr); // Output will be "WORLD"
+        }
+    }
+    
