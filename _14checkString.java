5422ff69c94cc06a88b2846bc7f2afe6f53ad421
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-09-07 21:30:20 +0530

    Add files via upload

---
+++ _14checkString.java
@@ -0,0 +1,7 @@
+ class Main {
+    public static void main(String[] args) {
+        String str = "World";
+        boolean result = str.endsWith("ld");
+        System.out.println(result); // Output: true
+    }
+}
---
+++ _15SplitString.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String[] parts = str.split(" ");
+        System.out.println(parts[0]); // Output: Hello
+        System.out.println(parts[1]); // Output: World
+    }
+}
+
