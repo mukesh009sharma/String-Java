ac14885a8e94b11869b5d82ffdc46fee938bab08
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-09-12 18:51:27 +0530

    Add files via upload

---
+++ _32CheckContainCharcater.java
@@ -0,0 +1,8 @@
+ class CheckSubstring {
+    public static void main(String[] args) {
+        String mainString = "OpenAI";
+        String subString = "AI";
+        boolean contains = mainString.contains(subString);
+        System.out.println("Does 'OpenAI' contain 'AI'? " + contains);
+    }
+}
