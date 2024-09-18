745308500fe903ca51807c017443954ebb6ed290
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-09-18 21:42:15 +0530

    Add files via upload

---
+++ _50CheckLowercase.java
@@ -0,0 +1,10 @@
+class _50CheckLowercase {
+    public static void main(String[] args) {
+        String str = "python";
+        if (str.equals(str.toLowerCase())) {
+            System.out.println("The string is in lowercase.");
+        } else {
+            System.out.println("The string is not in lowercase.");
+        }
+    }
+}
