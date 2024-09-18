6e30cf04d4ddebe314c33e58a487bf3fbd23b2d0
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-09-18 21:41:42 +0530

    Add files via upload

---
+++ _52BinaryToDecimal.java
@@ -0,0 +1,8 @@
+ class _52BinaryToDecimal {
+    public static void main(String[] args) {
+        String binaryStr = "1101";
+        int decimal = Integer.parseInt(binaryStr, 2);
+        System.out.println("Decimal representation of binary '1101': " + decimal);
+    }
+}
+
