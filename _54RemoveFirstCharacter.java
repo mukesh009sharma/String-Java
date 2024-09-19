06789a06d81609fb7fd54c70a9d7252cfbe3ca87
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-09-19 21:40:02 +0530

    Add files via upload

---
+++ _54RemoveFirstCharacter.java
@@ -0,0 +1,7 @@
+ class _54RemoveFirstCharacter {
+    public static void main(String[] args) {
+        String str = "delete";
+        String result = str.substring(1);
+        System.out.println("After removing first character: " + result);
+    }
+}
